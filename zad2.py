04da554d17308550e206af3ab97ca1c39d06359e
Michał Dziarkowski <115870394+mdziaro@users.noreply.github.com>
2022-12-02 16:17:08 +0100

    day 2

---
+++ zad2.py
@@ -0,0 +1,32 @@
+plik = open("input2.txt")
+dane = plik.readlines()
+plik.close()
+WYBOR = [0,"A","B","C","A","B","C"]
+ODP = [0,"X","Y","Z","X","Y","Z"]
+
+
+for i in range(len(dane)):
+    if dane[i][-1] == "\n":
+        dane[i] = dane[i][:-1]
+    dane[i] = dane[i].split(" ")
+print(dane)
+suma = 0
+
+#for i in range(len(dane)):
+#    if WYBOR.index(dane[i][0]) == ODP.index(dane[i][1]):
+#        suma += 3+ODP.index(dane[i][1])
+#    elif WYBOR.index(dane[i][0]) == ODP.index(dane[i][1])-1 or WYBOR.index(dane[i][0]) == ODP.index(dane[i][1])+2:
+#        suma += 6+ODP.index(dane[i][1])
+#    elif WYBOR.index(dane[i][0]) == ODP.index(dane[i][1])+1 or WYBOR.index(dane[i][0]) == ODP.index(dane[i][1])-2:
+#        suma += ODP.index(dane[i][1])
+#print(suma)
+suma = 0
+for i in range(len(dane)):
+    if dane[i][1] == "X":
+        suma += WYBOR.index(WYBOR[(WYBOR.index(dane[i][0])+2)])
+    elif dane[i][1] == "Y":
+        suma += 3+WYBOR.index(WYBOR[(WYBOR.index(dane[i][0]))])
+    elif dane[i][1] == "Z":
+        suma += 6+WYBOR.index(WYBOR[(WYBOR.index(dane[i][0])+1)])
+print(suma)
+
